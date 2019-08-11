1df02bf81a68aadb02dedef1343ab589f984f30c
ubelhj <ubelhj@uw.edu>
2019-08-10 17:45:46 -0700

    working MainActivity

--- app/src/main/java/com/ubelhj/randomnumbergenerator/MainActivity.java
+++ app/src/main/java/com/ubelhj/randomnumbergenerator/MainActivity.java
@@ -3,6 +3,12 @@ package com.ubelhj.randomnumbergenerator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mainButton = findViewById(R.id.button);
+        final EditText minimum = findViewById(R.id.minimum);
+        final EditText maximum = findViewById(R.id.maximum);
+        final TextView result = findViewById(R.id.resultTextView);
+
+        mainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random rand = new Random();
+                String minInput = minimum.getText().toString();
+                String maxInput = maximum.getText().toString();
+
+//                System.out.println(minInput);
+//                System.out.println(maxInput);
+
+                int minInt = Integer.parseInt(minInput);
+                int maxInt = Integer.parseInt(maxInput);
+
+//                System.out.println(minInt);
+//                System.out.println(maxInt);
+
+                if (minInt < 0) {
+                    return;
+                }
+
+                if (maxInt < minInt) {
+                    return;
+                }
+
+                Integer value = rand.nextInt(maxInt - minInt) + minInt;
+
+                System.out.println(value);
+
+                //String valueString = "" + value;
+
+                //System.out.println(valueString);
+
+                result.setText(value.toString());
+
+            }
+        });
     }
 }
